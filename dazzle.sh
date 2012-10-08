fa7181e1412350e362d4415303190eef0917e098
Robert C. Sheets <rcsheets@acm.org>
2012-10-08 00:29:30 -0700

    Working on making the script less linux-centric as well as not requiring bash

--- dazzle.sh
+++ dazzle.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # This program is free software. It comes without any warranty, to
 # the extent permitted by applicable law. You can redistribute it
@@ -6,20 +6,87 @@
 # To Public License, Version 2, as published by Sam Hocevar. See
 # http://sam.zoy.org/wtfpl/COPYING for more details.
 
+set -e
+
+# Set this to a non-empty value to enable trace output (for debugging)
+TRACE="yep"
+
+trace () {
+  if test -n "$TRACE"
+  then
+    echo "[TRACE] $*"
+  fi
+}
+
+# This script expects to be run as root. If we're not root, bail out.
+UID=$(id -u)
+if test x$UID != x0
+then
+  echo This script must be run as root.
+  exit 1
+fi
 
 # OS detection
-OS="debian"
-if [ -f "/etc/redhat-release" ]; then
-  OS="redhat"
+OS=$(uname)
+
+if test x$OS = xLinux
+then
+  if test -f "/etc/redhat-release"
+  then
+    DIST="redhat"
+  elif test -f "/etc/debian_version"
+  then
+    DIST="debian"
+  else
+    DIST="unknown"
+  fi
 fi
-  
+
+OSMESSAGE="Seems to be $OS"
+if test -n "$DIST"
+then
+  OSMESSAGE="$OSMESSAGE ($DIST)"
+fi
+trace $OSMESSAGE
+
 # Define text styles
-BOLD=`tput bold`
-DIM=`tput dim`
-NORMAL=`tput sgr0`
+BOLD=""
+DIM=""
+NORMAL=""
 
+if tput bold > /dev/null
+then
+  BOLD=$(tput bold)
+fi
 
-function create_account {
+if tput dim > /dev/null
+then
+  DIM=$(tput dim)
+fi
+
+if tput sgr0 > /dev/null
+then
+  NORMAL=$(tput sgr0)
+fi
+
+if test -z "$BOLD" -o -z "$NORMAL"
+then
+  trace "No support for bold text."
+  BOLD=""
+fi
+
+if test -z "$DIM" -o -z "$NORMAL"
+then
+  trace "No support for dim text."
+  DIM=""
+fi
+
+if test -z "$BOLD" -a -z "$DIM"
+then
+  NORMAL=""
+fi
+
+create_account () {
   echo "${BOLD}(1/4) Creating account \"storage\"...${NORMAL}"
   
   if [ `grep "^storage:" /etc/passwd | cut -b -7` = "storage" ]; then
@@ -32,7 +99,7 @@ function create_account {
   sleep 0.5
 }
 
-function configure_ssh {
+configure_ssh () {
   echo "${BOLD}(2/4) Configuring account \"storage\"...${NORMAL}"
   
   echo "${DIM} -> mkdir /home/storage/.ssh${NORMAL}"
@@ -58,7 +125,7 @@ function configure_ssh {
   sleep 0.5
 }
 
-function restart_ssh {
+restart_ssh () {
   echo "${BOLD}(3/4) Restarting SSH service...${NORMAL}"
   
   if [ "$OS" = "redhat" ]; then
@@ -70,7 +137,7 @@ function restart_ssh {
   fi
 }
 
-function install_git {
+install_git () {
   echo "${BOLD}(4/4) Installing Git package...${NORMAL}"
 
   if [ -f "/usr/bin/git" ]; then
@@ -87,7 +154,7 @@ function install_git {
   fi
 }
 
-function create_project {
+create_project () {
   echo "${BOLD}Creating project \"$1\"...${NORMAL}"
 
   if [ -f "/home/storage/$1/HEAD" ]; then
@@ -126,7 +193,7 @@ function create_project {
   echo
 }
 
-function link_client {
+link_client () {
   echo "Paste the contents of ${BOLD}\"~/SparkleShare/Your Name's link code.txt\"${NORMAL}"
   echo "(found on the client) into the field below and press ${BOLD}<ENTER>${NORMAL}."
   echo 
@@ -170,10 +237,22 @@ case $1 in
     echo
     echo "Usage: dazzle [COMMAND]"
     echo 
-    echo "  setup                            configures this machine to serve as a SparkleShare host"
-    echo "  create PROJECT_NAME              creates a SparkleShare project called PROJECT_NAME"
-    echo "  create-encrypted PROJECT_NAME    creates an encrypted SparkleShare project"
-    echo "  link                             links a SparkleShare client to this host by entering a link code"
+    echo "COMMAND can be one of the following:"
+    echo
+    echo "  setup"
+    echo "    configures this machine to serve as a SparkleShare host"
+    echo
+    echo "  create PROJECT_NAME"
+    echo "    creates a SparkleShare project called PROJECT_NAME"
+    echo
+    echo "  create-encrypted PROJECT_NAME"
+    echo "    creates an encrypted SparkleShare project"
+    echo
+    echo "  link"
+    echo "    links a SparkleShare client to this host by entering a link code"
+    echo
+    echo "  help"
+    echo "    show this help message"
     echo
     ;;
 esac
